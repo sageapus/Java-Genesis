ff148fa3ce0e9ea98b3cd7d6a764b7369ad4577c
Lusanda Mposula <lumposujhb024@student.wethinkcode.co.za>
2025-05-14 17:08:42 +0200

    made a conversation

--- src/main/java/org/example/HelloDialog.java
+++ src/main/java/org/example/HelloDialog.java
@@ -4,7 +4,16 @@ import javax.swing.JOptionPane;
 
 public class HelloDialog {
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "Hi Phethi!\nHow are you?");
-        JOptionPane.showMessageDialog(null, "Your first dialog!");
+        int result = JOptionPane.showConfirmDialog(null, "Do you want to continue?");
+        if (result == JOptionPane.YES_OPTION){
+            System.out.println("Okay!");
+            String name=JOptionPane.showInputDialog("Enter your name:");
+            JOptionPane.showMessageDialog(null,"Hello "+name);
+        }else{
+            JOptionPane.showMessageDialog(null,"Goodbye!");
+        }
+
+//        JOptionPane.showMessageDialog(null, "Hi Phethi!");
+//        JOptionPane.showMessageDialog(null, "Your first dialog!");
     }
 }
